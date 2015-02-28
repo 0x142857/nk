70c6b27228bab5716b763df1ece30f2c0dec438c
Kchan Zen <kchanzen@gmail.com>
2015-02-28 16:32:41 +0800

    REMOVE jade watch and update gulp version

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,7 @@ var gulp = require('gulp'),
 
 var paths = {
   css: ['./public/css/*.css'],
-  styl: ['./src/styl/*.styl'],
-  jade: ['./template/*.jade']
+  styl: ['./src/styl/*.styl']
 };
 
 gulp.task('server', function () {
@@ -28,15 +27,10 @@ gulp.task('css', function () {
   return gulp.src(paths.css);
 });
 
-gulp.task('jade', function () {
-  return gulp.src(paths.jade);
-});
-
 gulp.task('watch', function() {
 
   gulp.watch(paths.styl, ['styl']);
   gulp.watch(paths.css, ['css']);
-  gulp.watch(paths.jade,['jade']);
   gulp.watch(['app.js'], [server.run]);
 
 });
